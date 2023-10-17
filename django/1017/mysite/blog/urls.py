440b3d3fb7f3b8db27c88a49a78882bf0c2e33e9
su <mini72667@gmail.com>
2023-10-17 14:36:22 +0900

    1017

---
+++ django/1017/mysite/blog/urls.py
@@ -0,0 +1,10 @@
+# blog > urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.postlist, name='postlist'),
+    path('<int:pk>/', views.postdetail, name='postdetail'),
+    path('tag/<str:tag>/', views.posttag, name='posttag'),
+]
