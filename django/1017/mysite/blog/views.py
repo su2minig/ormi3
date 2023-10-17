cae2e283aa3ab13fd86866c471dc4a2fa9ffb4f8
su2minig <141402694+su2minig@users.noreply.github.com>
2023-10-17 13:42:53 +0900

    1017 블로그 포스트 댓글 태그 생성

---
+++ django/1017/mysite/blog/views.py
@@ -0,0 +1,8 @@
+# blog > views.py
+
+from django.shortcuts import render
+from .models import Post
+
+def postlist(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/postlist.html', {'posts':posts})
