3311aaf0cf0231d391dd0bf72f1b8a1df7a590b9
su <mini72667@gmail.com>
2023-10-20 12:18:46 +0900

    1020-1

---
+++ django/1020/account_auth/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.postlist, name='postlist'),
+    path('register/', views.userregister, name='userregister'),
+    path('login/', views.userlogin, name='userlogin'),
+]
---
+++ django/1020/router/tutorialdjango/urls.py
@@ -0,0 +1,8 @@
+# tutorialdjango > urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('book/', include('book.urls')),
+]
